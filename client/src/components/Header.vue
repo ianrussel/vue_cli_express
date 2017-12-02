af6c92299867f2eee2ade985322474da6454b950
ianrussel <ian.russel@yahoo.com>
2017-12-02 17:13:58 +0800

    removed user

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -9,7 +9,7 @@
         </button>
         <b-navbar-nav class="d-md-down-none">
             <b-nav-item class="px-3">Dashboard</b-nav-item>
-            <b-nav-item class="px-3">Users</b-nav-item>
+            <b-nav-item class="px-3">User</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
             <HeaderDropdown/>
