c5744439e12fc3fae2abbf1c9050f5cad35e2c7c
engageiq-ianrussel <ianrussel@engageiq.com>
2017-12-11 10:03:33 +0800

    added revert auth0 credentials to dev

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,8 +1,9 @@
 module.exports = {
     NODE_ENV: '"production"',
-    CLIENTID: '"51re13he1en9TTW0JsLVCPYlwuTQAgmS"',
-    REDIRECTS: '"http://localhost:8081/callback"',
+    CLIENTID: '"mPDD8oNcMeKfbhCIzhgpWS0yqZgSGRuz"',
+    REDIRECTS: '"https://vuejs-ian.herokuapp.com/callback"',
     SCOPES: '"openid profile email roles"',
     AUDIENCES: '"https://ianrussel.auth0.com/userinfo"',
     CLIENTDOMAIN: '"ianrussel.auth0.com"'
 }
+
