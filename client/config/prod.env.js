c377149523c508685b28a8161f2fac916e75ee03
engageiq-ianrussel <ianrussel@engageiq.com>
2017-12-12 11:35:19 +0800

    remove production config for auth0

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,19 +1,10 @@
 // dev
-// module.exports = {
-//     NODE_ENV: '"production"',
-//     CLIENTID: '"51re13he1en9TTW0JsLVCPYlwuTQAgmS"',
-//     REDIRECTS: '"http://localhost:8081/callback"',
-//     SCOPES: '"openid profile email roles"',
-//     AUDIENCES: '"https://ianrussel.auth0.com/userinfo"',
-//     CLIENTDOMAIN: '"ianrussel.auth0.com"'
-// }
-// prod
-
 module.exports = {
     NODE_ENV: '"production"',
-    CLIENTID: '"mPDD8oNcMeKfbhCIzhgpWS0yqZgSGRuz"',
-    REDIRECTS: '"https://vuejs-ian.herokuapp.com/callback"',
+    CLIENTID: '"51re13he1en9TTW0JsLVCPYlwuTQAgmS"',
+    REDIRECTS: '"http://localhost:8081/callback"',
     SCOPES: '"openid profile email roles"',
-    AUDIENCES: '"https://ianrussel.auth0.com/userinfo"',
+    AUDIENCES: '"https://ian.auth0.com/userinfo"',
     CLIENTDOMAIN: '"ianrussel.auth0.com"'
 }
+// prod
